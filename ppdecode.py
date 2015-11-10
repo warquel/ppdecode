9542360a49d2c2a21d0510c5be0540a46becb909
Warren Raquel <warquel@gmail.com>
2015-11-10 08:38:32 -0600

    Removing useless line

--- ppdecode.py
+++ ppdecode.py
@@ -3,7 +3,6 @@ import urlparse, re
 class ppdecode(object):
     def __init__(self, url):
         self.pplink = url
-        self._decodeurl, url
         self.arguments = urlparse.parse_qs(urlparse.urlparse(url).query)
         self.url = self._decodeurl()
         self._parse()
